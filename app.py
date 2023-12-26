81da3127e7b80f7e401531cce65cfea08d0c25eb
jim-les <jimleston35@gmail.com>
2023-12-26 20:16:33 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def under_development():
+    return render_template('under_development.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
